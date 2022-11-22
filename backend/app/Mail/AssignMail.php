bcd507195f0c5b99159c6319fb855b713da69171
Asahi Kawamura <kyorozon@gmail.com>
2022-11-22 13:55:23 +0900

    メール送信機能の追加

---
+++ backend/app/Mail/AssignMail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class AssignMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($name, $email)
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->to($this->email)
+            ->subject('タスクの割り当て')
+            ->view('test.assign')
+            ->with([
+                'name' => $this->name,
+            ]);
+    }
+}
